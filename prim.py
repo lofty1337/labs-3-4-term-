6ea50d53a35e92fbacd2171dadabb2c306e910d1
lofty1337 <90724908+lofty1337@users.noreply.github.com>
2023-05-16 21:23:47 +0300

    Update prim.py

--- prim.py
+++ prim.py
@@ -1,61 +1,38 @@
-import sys
+def prim(graph):
+    # Создаем пустое минимальное покрывающее дерево и множество вершин, уже включенных в дерево
+    minimum_spanning_tree = {}
+    visited = set()
 
+    # Выбираем произвольную начальную вершину
+    start_vertex = list(graph.keys())[0]
+    visited.add(start_vertex)
 
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
+    while len(visited) < len(graph):
+        min_weight = float('inf')
+        min_edge = None
 
-    def add_edge(self, u, v, w):
-        self.graph[u][v] = w
-        self.graph[v][u] = w
+        # Поиск ребра минимального веса, которое соединяет посещенную вершину с непосещенной вершиной
+        for vertex in visited:
+            for neighbor, weight in graph[vertex].items():
+                if neighbor not in visited and weight < min_weight:
+                    min_weight = weight
+                    min_edge = (vertex, neighbor)
 
-    def min_key(self, key, mst_set):
-        min_value = sys.maxsize
-        min_index = None
+        # Добавляем найденное ребро в минимальное покрывающее дерево
+        if min_edge:
+            vertex, neighbor = min_edge
+            minimum_spanning_tree[vertex] = {neighbor: min_weight}
+            visited.add(neighbor)
 
-        for v in range(self.V):
-            if key[v] < min_value and not mst_set[v]:
-                min_value = key[v]
-                min_index = v
-
-        return min_index
-
-    def prim(self):
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V
-        key[0] = 0
-        mst_set = [False] * self.V
-
-        for _ in range(self.V):
-            u = self.min_key(key, mst_set)
-            mst_set[u] = True
-
-            for v in range(self.V):
-                if (
-                    self.graph[u][v] > 0
-                    and not mst_set[v]
-                    and self.graph[u][v] < key[v]
-                ):
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
-
-        mst = []
-        for i in range(1, self.V):
-            mst.append((parent[i], i, self.graph[i][parent[i]]))
-
-        return mst
-
+    return minimum_spanning_tree
 
 # Пример использования
-g = Graph(4)
-g.add_edge(0, 1, 10)
-g.add_edge(0, 2, 6)
-g.add_edge(0, 3, 5)
-g.add_edge(1, 3, 15)
-g.add_edge(2, 3, 4)
-minimum_spanning_tree = g.prim()
+graph = {
+    'A': {'B': 4, 'C': 2},
+    'B': {'A': 4, 'C': 1, 'D': 5},
+    'C': {'A': 2, 'B': 1, 'D': 8},
+    'D': {'B': 5, 'C': 8}
+}
 
-print("Минимальное покрывающее дерево:")
-for u, v, weight in minimum_spanning_tree:
-    print(f"{u} -- {v}    Вес: {weight}")
+minimum_spanning_tree = prim(graph)
+print(minimum_spanning_tree)
