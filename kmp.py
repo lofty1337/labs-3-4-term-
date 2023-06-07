e166f5126e125f918baa230fea65b3ec657c315c
Alex <90724908+lofty1337@users.noreply.github.com>
2023-06-07 16:40:25 +0300

    Create kmp.py

---
+++ kmp.py
@@ -0,0 +1,49 @@
+def compute_prefix_function(pattern):
+    m = len(pattern)
+    pi = [0] * m
+    k = 0
+
+    for q in range(1, m):
+        while k > 0 and pattern[k] != pattern[q]:
+            k = pi[k - 1]
+
+        if pattern[k] == pattern[q]:
+            k += 1
+
+        pi[q] = k
+
+    return pi
+
+
+def search(pattern, text):
+    n = len(text)
+    m = len(pattern)
+    pi = compute_prefix_function(pattern)
+    q = 0
+    result = []
+
+    for i in range(n):
+        while q > 0 and pattern[q] != text[i]:
+            q = pi[q - 1]
+
+        if pattern[q] == text[i]:
+            q += 1
+
+        if q == m:
+            result.append(i - m + 1)
+            q = pi[q - 1]
+
+    return result
+
+
+# Пример использования
+text = "ABABDABACDABABCABAB"
+pattern = "ABABC"
+
+matches = search(pattern, text)
+
+if len(matches) > 0:
+    print("Образец найден в следующих позициях:")
+    print(matches)
+else:
+    print("Образец не найден")
