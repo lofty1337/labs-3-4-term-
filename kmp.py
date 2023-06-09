407766a8e19e0a4ca34025bdf11551270e91cd3a
Alex <90724908+lofty1337@users.noreply.github.com>
2023-06-09 14:06:29 +0300

    Update kmp.py

--- kmp.py
+++ kmp.py
@@ -1,16 +1,16 @@
 def compute_prefix_function(pattern):
     m = len(pattern)
-    pi = [0] * m
+    pi = [0] * m  # Создаем список pi с нулевыми значениями
     k = 0
 
     for q in range(1, m):
         while k > 0 and pattern[k] != pattern[q]:
-            k = pi[k - 1]
+            k = pi[k - 1]  # Возвращаемся к предыдущему префиксу
 
         if pattern[k] == pattern[q]:
             k += 1
 
-        pi[q] = k
+        pi[q] = k  # Записываем длину наибольшего префикса, который является суффиксом
 
     return pi
 
@@ -18,20 +18,20 @@ def compute_prefix_function(pattern):
 def search(pattern, text):
     n = len(text)
     m = len(pattern)
-    pi = compute_prefix_function(pattern)
-    q = 0
-    result = []
+    pi = compute_prefix_function(pattern)  # Вычисляем префикс-функцию для образца
+    q = 0  # Текущий индекс в образце
+    result = []  # Список для хранения позиций, где найден образец
 
     for i in range(n):
         while q > 0 and pattern[q] != text[i]:
-            q = pi[q - 1]
+            q = pi[q - 1]  # Возвращаемся к предыдущему префиксу
 
         if pattern[q] == text[i]:
             q += 1
 
         if q == m:
-            result.append(i - m + 1)
-            q = pi[q - 1]
+            result.append(i - m + 1)  # Образец найден в позиции i-m+1
+            q = pi[q - 1]  # Продолжаем поиск следующих вхождений образца
 
     return result
 
